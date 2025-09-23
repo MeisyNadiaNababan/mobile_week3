f59a33525231628cdc8bf6f75ba9a1403444b532
MeisyNadiaNababan <meisynadianababan@gmail.com>
2025-09-23 19:27:02 +0700

    first commit

---
+++ bin/mobile_week3.dart
@@ -0,0 +1,5 @@
+import 'package:mobile_week3/mobile_week3.dart' as mobile_week3;
+
+void main(List<String> arguments) {
+  print('Hello world: ${mobile_week3.calculate()}!');
+}
---
+++ test/mobile_week3_test.dart
@@ -0,0 +1,8 @@
+import 'package:mobile_week3/mobile_week3.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
